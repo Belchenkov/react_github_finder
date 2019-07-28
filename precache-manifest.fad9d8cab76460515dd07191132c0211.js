b568e8afb3bee4a6497be4a482b6b68c8d330759
Aleksey Belchenkov <u608110@gmail.com>
2019-07-28 16:01:16 +0300

    Updates

---
+++ precache-manifest.fad9d8cab76460515dd07191132c0211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bf875050c6a4db0dc7246100b494af",
+    "url": "/react_github_finder/index.html"
+  },
+  {
+    "revision": "1affc2371bd65a653d7d",
+    "url": "/react_github_finder/static/css/main.399ec4a0.chunk.css"
+  },
+  {
+    "revision": "858a31191b1f7829b857",
+    "url": "/react_github_finder/static/js/2.2bbc5e60.chunk.js"
+  },
+  {
+    "revision": "1affc2371bd65a653d7d",
+    "url": "/react_github_finder/static/js/main.aad2ef23.chunk.js"
+  },
+  {
+    "revision": "0407175392c1c0f1dc82",
+    "url": "/react_github_finder/static/js/runtime~main.37c5b991.js"
+  }
+]);
